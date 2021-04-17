d76146931070e25b805e4aad693acded14c51360
project <fd2.croson.jordan@gmail.com>
2021-04-17 15:33:50 +0100

    Combined controller running

--- controllers/footbot_flocking/footbot_flocking.cpp
+++ controllers/footbot_flocking/footbot_flocking.cpp
@@ -1,6 +1,9 @@
 /* Include the controller definition */
 #include "footbot_flocking.h"
 /* Function definitions for XML parsing */
+
+/* 2D vector definition */
+#include <argos3/core/utility/math/vector2.h>
 #include <argos3/core/utility/configuration/argos_configuration.h>
 #include <argos3/core/utility/logging/argos_log.h>
 
@@ -56,7 +59,13 @@ CFootBotFlocking::CFootBotFlocking() :
    m_pcWheels(NULL),
    m_pcLight(NULL),
    m_pcLEDs(NULL),
-   m_pcCamera(NULL) {}
+   m_pcCamera(NULL),
+   m_pcProximity(NULL),
+   m_cAlpha(10.0f),
+   m_fDelta(0.5f),
+   m_fWheelVelocity(2.5f),
+   m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
+                           ToRadians(m_cAlpha)) {}
 
 /****************************************/
 /****************************************/
@@ -87,6 +96,19 @@ void CFootBotFlocking::Init(TConfigurationNode& t_node) {
    m_pcLight  = GetSensor  <CCI_FootBotLightSensor                    >("footbot_light");
    m_pcLEDs   = GetActuator<CCI_LEDsActuator                          >("leds");
    m_pcCamera = GetSensor  <CCI_ColoredBlobOmnidirectionalCameraSensor>("colored_blob_omnidirectional_camera");
+
+   m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity"    );
+   /*
+    * Parse the configuration file
+    *
+    * The user defines this part. Here, the algorithm accepts three
+    * parameters and it's nice to put them in the config file so we don't
+    * have to recompile if we want to try other settings.
+    */
+   GetNodeAttributeOrDefault(t_node, "alpha", m_cAlpha, m_cAlpha);
+   m_cGoStraightAngleRange.Set(-ToRadians(m_cAlpha), ToRadians(m_cAlpha));
+   GetNodeAttributeOrDefault(t_node, "delta", m_fDelta, m_fDelta);
+   GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
    /*
     * Parse the config file
     */
@@ -114,6 +136,31 @@ void CFootBotFlocking::Init(TConfigurationNode& t_node) {
 
 void CFootBotFlocking::ControlStep() {
    SetWheelSpeedsFromVector(VectorToLight() + FlockingVector());
+   const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
+   /* Sum them together */
+   CVector2 cAccumulator;
+   for(size_t i = 0; i < tProxReads.size(); ++i) {
+      cAccumulator += CVector2(tProxReads[i].Value, tProxReads[i].Angle);
+   }
+   cAccumulator /= tProxReads.size();
+   /* If the angle of the vector is small enough and the closest obstacle
+    * is far enough, continue going straight, otherwise curve a little
+    */
+   CRadians cAngle = cAccumulator.Angle();
+   if(m_cGoStraightAngleRange.WithinMinBoundIncludedMaxBoundIncluded(cAngle) &&
+      cAccumulator.Length() < m_fDelta ) {
+      /* Go straight */
+      m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
+   }
+   else {
+      /* Turn, depending on the sign of the angle */
+      if(cAngle.GetValue() > 0.0f) {
+         m_pcWheels->SetLinearVelocity(m_fWheelVelocity, 0.0f);
+      }
+      else {
+         m_pcWheels->SetLinearVelocity(0.0f, m_fWheelVelocity);
+      }
+   }
 }
 
 /****************************************/
@@ -272,5 +319,34 @@ void CFootBotFlocking::SetWheelSpeedsFromVector(const CVector2& c_heading) {
  * The string is then usable in the XML configuration file to refer to this controller.
  * When ARGoS reads that string in the XML file, it knows which controller class to instantiate.
  * See also the XML configuration files for an example of how this is used.
+ */
+
+
+
+
+
+/****************************************/
+/****************************************/
+
+   
+
+/****************************************/
+/****************************************/
+
+/****************************************/
+/****************************************/
+
+/****************************************/
+/****************************************/
+
+/*
+ * This statement notifies ARGoS of the existence of the controller.
+ * It binds the class passed as first argument to the string passed as
+ * second argument.
+ * The string is then usable in the configuration file to refer to this
+ * controller.
+ * When ARGoS reads that string in the configuration file, it knows which
+ * controller class to instantiate.
+ * See also the configuration files for an example of how this is used.
  */
 REGISTER_CONTROLLER(CFootBotFlocking, "footbot_flocking_controller")
